2c8cb559886e044d30c065ea4ac85f98493e6bd4
Brian Kariuki <107594995+DevBrianKariuki@users.noreply.github.com>
2024-02-08 02:50:58 +0300

    Final Commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,32 +3,38 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="w-full flex mt-32 items-center justify-center font-inter">
-      <div className="w-[90%] flex flex-col items-center">
+      <div className="w-full flex flex-col items-center">
         <h2 className="text-white font-bold text-3xl text-center">Contact</h2>
-        <div className="flex items-center gap-4 justify-center sm:w-[60%] flex-col">
-          <form className="my-6 " action="#">
+        <div className="flex w-full items-center gap-4 justify-center sm:w-[60%] flex-col">
+          <form
+            className="my-6 items-center justify-center flex flex-col"
+            action="#"
+          >
             <div className="flex flex-col gap-4 w-full sm:flex-row">
               <input
-                className=" w-full sm:w-[50%] px-3 py-3 rounded-md bg-neutral-300"
+                className="outline-none focus:ring-[2px] focus:ring-yellow w-full sm:w-[50%] px-3 py-3 rounded-md bg-neutral-300"
                 placeholder="First Name"
               />
               <input
-                className=" w-full sm:w-[50%] px-3 py-3 rounded-md bg-neutral-300"
+                className="outline-none focus:ring-[2px] focus:ring-yellow w-full sm:w-[50%] px-3 py-3 rounded-md bg-neutral-300"
                 placeholder="Last Name"
               />
             </div>
             <input
-              className=" w-full mt-4 px-3 py-3 rounded-md bg-neutral-300"
+              className="outline-none focus:ring-[2px] focus:ring-yellow w-full mt-4 px-3 py-3 rounded-md bg-neutral-300"
               placeholder="Phone(Optional)"
             />
             <input
-              className=" w-full mt-4 px-3 py-3 rounded-md bg-neutral-300"
+              className="outline-none focus:ring-[2px] focus:ring-yellow w-full mt-4 px-3 py-3 rounded-md bg-neutral-300"
               placeholder="Email"
             />
             <textarea
-              className=" w-full mt-4 px-3 py-3 rounded-md bg-neutral-300"
+              className="outline-none focus:ring-[2px] focus:ring-yellow w-full mt-4 px-3 py-3 rounded-md bg-neutral-300"
               placeholder="Message"
             />
+            <button className="bg-lightblue justify-center sm:w-[40%] my-3 py-3 rounded-lg text-lg font-bold w-full">
+              Send
+            </button>
           </form>
         </div>
       </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { FaRegCopyright } from "react-icons/fa6";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full mt-6 flex items-center flex-col gap-6 justify-center font-inter">
+      <div className="w-[90%] sm:w-[50%] grid gap-12 grid-cols-2 sm:grid-cols-4">
+        <div className="flex flex-col gap-3">
+          <h1 className="font-bold text-white text-lg">Lorem</h1>
+          <ul className="list-none flex flex-col gap-2">
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="font-bold text-white text-lg">Lorem</h1>
+          <ul className="list-none flex flex-col gap-2">
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="font-bold text-white text-lg">Lorem</h1>
+          <ul className="list-none flex flex-col gap-2">
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="font-bold text-white text-lg">Lorem</h1>
+          <ul className="list-none flex flex-col gap-2">
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+            <li className="font-inter text-white">Ipsum</li>
+          </ul>
+        </div>
+      </div>
+      <p className="font-inter gap-2 font-bold flex items-center justify-center text-white">
+        <FaRegCopyright /> 2024
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
